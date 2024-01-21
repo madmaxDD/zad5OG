88cb2d0caecaa79143fd88206ff1388756e97063
s18774 <s18774@pjwstk.edu.pl>
2024-01-21 10:52:46 +0100

    clean the code

--- server.js
+++ server.js
@@ -60,43 +60,6 @@ app.delete('/results/:id', (req,res)=>{
    }
    res.json(users)
 })
-/*
-app.post('/submit', [
-  check('imie').isLength({ min: 2, max:20 }).withMessage('Imię musi zawierać co najmniej 2 znaki i nie więcej niż 20'),
-  check('wiek').custom(value => {
-    if (isNaN(value) || value < 18 || value >100) {
-      throw new Error('Musisz mieć co najmniej 18 lat, ale też nie więcej niż 100 xd');
-    }
-    return true;
-  }),
-  check('email').isEmail().withMessage('Niepoprawny format adresu email.'),
-], (req, res) => {
-  const { imie, wiek, email } = req.body;
-
-  const errors = validationResult(req);
-  if (!errors.isEmpty()){
-    var errorLog = `Date and Time: ${new Date().toLocaleString()}\tMethod: ${req.method}\tURL: ${req.originalUrl}\tError: `;
-    errors.array().forEach(error => {
-      errorLog += `${error.msg}\t`;
-    });
-  
-    errorLog += '\n';
-    fs.appendFile(path.join(__dirname,'errorLogs', 'erorlogs.txt'),errorLog, (err)=>{
-    if(err) throw err;
-    console.log('write complete')
-    })
-  }
-
-  if (errors.isEmpty()) {
-     return res.render('results', { imie, wiek, email }); 
-  }
-
-  return res.render('form', {
-    errors: errors.array()
-  });
-});
-
-*/
 
 app.listen(port, () => {
   console.log(`Serwer działa na porcie ${port}`);
